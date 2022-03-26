3d009b0d88367015adffb2487e744f55b2e2e137
rivavl <mvkalinenkova@yandex.ru>
2022-03-26 20:07:43 +0300

    add MainFragment and RegisterFragment classes and set listeners on buttons

---
+++ app/src/main/java/com/marina/vk2022/presentation/fragments/MainFragment.kt
@@ -0,0 +1,51 @@
+package com.marina.vk2022.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.marina.vk2022.R
+import com.marina.vk2022.databinding.FragmentMainBinding
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+        setupButtonListeners()
+    }
+
+    private fun setupButtonListeners() {
+
+        binding.startRecordImageBtn.setOnClickListener {
+            binding.startRecordImageBtn.visibility = View.GONE
+            binding.stopRecordImageBtn.visibility = View.VISIBLE
+            startRecord()
+        }
+
+        binding.stopRecordImageBtn.setOnClickListener {
+            binding.startRecordImageBtn.visibility = View.VISIBLE
+            binding.stopRecordImageBtn.visibility = View.GONE
+            stopRecord()
+        }
+    }
+
+    private fun startRecord() {
+
+    }
+
+    private fun stopRecord() {
+
+    }
+}
---
+++ app/src/main/java/com/marina/vk2022/presentation/fragments/RegisterFragment.kt
@@ -0,0 +1,41 @@
+package com.marina.vk2022.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.marina.vk2022.R
+import com.marina.vk2022.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRegisterBinding.bind(view)
+        setupButtonListeners()
+    }
+
+    private fun setupButtonListeners() {
+        //если не авторизуемся, сразу переходим на основной экран
+        binding.continueWithoutRegBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
+        }
+
+        //авторизация через вк
+        binding.registerWithVkBtn.setOnClickListener {
+            // TODO: 26.03.2022
+        }
+    }
+}
