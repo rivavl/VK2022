a10551132d45a45508e59533c514d2d535bbce5f
rivavl <mvkalinenkova@yandex.ru>
2022-03-26 20:09:12 +0300

    add entity for record item in domain layer

--- app/src/main/java/com/marina/vk2022/domain/entity/RecordItem.kt
+++ app/src/main/java/com/marina/vk2022/domain/entity/RecordItem.kt
@@ -1,16 +1,13 @@
 package com.marina.vk2022.domain.entity
 
-import androidx.room.ColumnInfo
-import androidx.room.PrimaryKey
-
 /*
 * Модель для бизнес логики
 * */
 
 data class RecordItem(
-    val recordId:Long = 0L,
-    var recordName:String ="",
-    var recordPath:String ="",
-    var recordLength:Long = 0L,
-    var recordCreatingTime:Long=0L
+    val recordId: Long = 0L,
+    var recordName: String = "",
+    var recordPath: String = "",
+    var recordLength: Long = 0L,
+    var recordCreatingTime: Long = 0L
 )
